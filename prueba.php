d7dc807dadbb4cb1a1e962fbd58ad392304dc8ed
fernanda1980 <144740609+fernanda1980@users.noreply.github.com>
2023-09-11 19:21:24 -0300

    carga inicial
    carga inicial

---
+++ prueba.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Mi primer programa de back</h1>
+    <?php
+    $saludo = "Hola Potrero" ;
+    echo $saludo;
+    $saludo = "como estas??";
+    echo $saludo;
+    ?>
+  </body>
+</html>
